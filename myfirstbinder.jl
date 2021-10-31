4f7647179db70b0ae47fb10f0bdfb7191e7cf087
Tomas J Aragon <tja@tomas-mbp.lan>
2021-10-30 22:49:03 -0700

    update

---
+++ myfirstbinder.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.17.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2d7c8ddc-3a0a-11ec-3c6c-dd16934484ad
+md"# My First Pluto Binder"
+
+# ╔═╡ 081cc7dd-418d-4038-8ab8-93b8efd7717a
+begin
+	x = 3
+	y = 4
+	z = x + y
+end
+
+# ╔═╡ Cell order:
+# ╠═2d7c8ddc-3a0a-11ec-3c6c-dd16934484ad
+# ╠═081cc7dd-418d-4038-8ab8-93b8efd7717a
